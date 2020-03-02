4ff86043b53e39691a14207c30dfd53ae38de92d
lughjw <lughjw@gmail.com>
2020-03-02 12:07:15 -0600

    Done

---
+++ app/models/appearance.rb
@@ -0,0 +1,8 @@
+class Appearance < ApplicationRecord
+    belongs_to :guest
+    belongs_to :episode
+
+    validates :guest, presence: true
+    validates :episode, presence: true
+    validates :rating, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5}
+end
